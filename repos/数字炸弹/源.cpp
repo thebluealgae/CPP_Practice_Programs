b2a490860b52aad6ebfc85c9f0626090afb16f32
TheBlueAlgae <feizhenyang369@163.com>
2022-10-19 12:38:18 +0800

    Add files via upload

--- repos/数字炸弹/源.cpp
+++ repos/数字炸弹/源.cpp
@@ -5,15 +5,16 @@ int main()
 {
 	int leftNumber = 0, rightNumber = 0, guessNumber = 0;	//初始化这几个值，注意必须要对变量进行初始化操作，否则可能会导致一些奇奇怪怪的问题！
 
-	std::cout << "Please enter two numbers that will be used to construct an interval:" << std::endl;	//让你规定一个区间。正确答案将从你给定的区间中随机指定。
+	std::cout << "Please enter two numbers that will be used to construct an interval:" << std::endl;	//规定一个区间。正确答案将从你给定的区间中随机指定。
 	std::cin >> leftNumber >> rightNumber;
 
 	if (leftNumber < rightNumber)
 	{
-		std::random_device rd;											//这几行
-		std::default_random_engine e(rd());								//是用来生成
-		std::uniform_int_distribution<int> u(leftNumber, rightNumber);	//含于用户规定的区间的
-		int answer = u(e);												//随机答案的。
+		std::random_device rd;
+		std::default_random_engine e(rd());
+		std::uniform_int_distribution<int> u(leftNumber, rightNumber);
+		int answer = u(e);
+		//上面四行是用来含于用户规定的区间的随机答案的。
 
 		std::cout << "Please guess a number!" << std::endl;
 		std::cin >> guessNumber;
@@ -31,6 +32,11 @@ int main()
 			{
 				rightNumber = guessNumber;
 				std::cout << "Now the number you will guess is located in [" << leftNumber << ", " << rightNumber << "]" << std::endl;
+			}
+
+			if (guessNumber < leftNumber || guessNumber > rightNumber)
+			{
+				std::cout << "The guess number must be in the interval you set! Please re-enter!" << std::endl;
 			}
 
 			std::cin >> guessNumber;
@@ -56,5 +62,4 @@ int main()
 	}
 }
 //0x0049 0x0020 0x006C 0x006F 0x0076 0x0065 0x0020 0x0052 0x0075 0x0069 0x0078 0x0075 0x0065 0x0020 0x0044 0x0075 0x0061 0x006E 0x0021
-//Well, there is a series of numbers. Perhaps you will figure out something interesting after successfully decoding these numbers...
-//If you indeed successfully decoded these numbers, DO NOT TELL ANYBODY ELSE THE ANSWER!
+//Perhaps you will figure out something interesting after successfully decoding these numbers...
