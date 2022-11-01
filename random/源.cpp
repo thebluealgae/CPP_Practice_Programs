45afc4b87a7d33263484fae70cd56dd59a0d6520
TheBlueAlgae <feizhenyang369@163.com>
2022-11-01 22:41:26 +0800

    Add files via upload

--- random/源.cpp
+++ random/源.cpp
@@ -3,21 +3,13 @@
 
 int main()
 {
-	int min = 0, max = 0;
+	int max = 0;
 	std::cout << "输入参与的人数：" << std::endl;
 	std::cin >> max;
 
-	const int p = max;
-
-	while (max > 1)
-	{
-		--max;
-	}
-	min = max;
-
 	std::random_device rd;
 	std::default_random_engine e(rd());
-	std::uniform_int_distribution<int> u(min, p);
+	std::uniform_int_distribution<int> u(1, max);
 
 	std::cout << u(e);
 
